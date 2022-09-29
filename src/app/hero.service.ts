4df27d6e967ec9566a48605f63fc5196738598e5
Joseph Vallejo <javallej@asu.edu>
2022-09-29 14:20:37 +0200

    Change from sync to async

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES} from './mock-heroes'
+import { Observable, of} from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,9 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes  = of(HEROES)
+    return heroes;
   }
 
 }
