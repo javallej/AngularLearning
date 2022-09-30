7889f92503fca956a02deb7528fa2655eebca7d8
Joseph Vallejo <javallej@asu.edu>
2022-09-30 15:20:49 +0200

    Final Section 5

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero} from "../hero";
-import {HeroService} from "../hero.service";
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
-  heroes:Hero[] = [];
+  heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
@@ -16,7 +16,8 @@ export class DashboardComponent implements OnInit {
     this.getHeroes();
   }
 
-  getHeroes():void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1,5));
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
